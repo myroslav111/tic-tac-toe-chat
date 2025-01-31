c72f836fa31fbb98328ef5d5c6a28c9ebc5cb5f8
start.vaadin.com <no-reply@vaadin.com>
2025-01-31 16:46:17 +0000

    Generated project

---
+++ src/main/java/com/firstgame/application/views/myview/MyViewView.java
@@ -0,0 +1,37 @@
+package com.firstgame.application.views.myview;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("My View")
+@Route("")
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn2.setJustifyContentMode(JustifyContentMode.CENTER);
+        layoutColumn2.setAlignItems(Alignment.CENTER);
+        layoutColumn3.setWidth("496px");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        layoutColumn3.setJustifyContentMode(JustifyContentMode.START);
+        layoutColumn3.setAlignItems(Alignment.START);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+    }
+}
